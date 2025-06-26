14380ccc2f0b4aaf4e530e05b6a28dfd22481e2d
Hernan Richasse <hernanrichasse@gmail.com>
2025-06-25 20:12:18 -0400

    se actualiza data.sql

--- microservice-venta/src/main/resources/data.sql
+++ microservice-venta/src/main/resources/data.sql
@@ -1,9 +1,18 @@
 
-INSERT INTO ventas (id, name, idventa, venta_id, producto_id)
-VALUES (1, 'Esteban', 'IDV-0001', 1001, 2001);
+CREATE TABLE IF NOT EXISTS ventas (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    idventa VARCHAR(255),
+    venta_id BIGINT,
+    producto_id BIGINT
+);
 
-INSERT INTO ventas (id, name, idventa, venta_id, producto_id)
-VALUES (2, 'María',  'IDV-0002', 1002, 2002);
 
-INSERT INTO ventas (id, name, idventa, venta_id, producto_id)
-VALUES (3, 'Carlos', 'IDV-0003', 1003, 2003);
+INSERT INTO ventas (name, idventa, venta_id, producto_id) 
+VALUES ('Esteban', 'IDV-0001', 1001, 2001);
+
+INSERT INTO ventas (name, idventa, venta_id, producto_id) 
+VALUES ('María', 'IDV-0002', 1002, 2002);
+
+INSERT INTO ventas (name, idventa, venta_id, producto_id) 
+VALUES ('Carlos', 'IDV-0003', 1003, 2003);
